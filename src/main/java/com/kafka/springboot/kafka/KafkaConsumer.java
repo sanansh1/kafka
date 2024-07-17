43eae895e54cdf89fd2c06e9624ab3a00e4f8c9a
Sandhya Kumari <158607087+sanansh1@users.noreply.github.com>
2024-07-17 12:26:46 -0400

    Initial checkin

---
+++ src/main/java/com/kafka/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.kafka.springboot.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+    @KafkaListener(topics = "sandhya", groupId = "myGroup")
+    public void consume(String message){
+        LOGGER.info(String.format("message received %s", message));
+
+    }
+}
